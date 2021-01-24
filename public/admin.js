99172f1b88a6f5c630fb1207a78383aaab5ea2a8
Josip Leko <josipleko@mail.com>
2021-01-24 17:20:00 +0100

    Modified admin.html and index.html

--- public/admin.js
+++ public/admin.js
@@ -1,218 +1,234 @@
-const addModerator = document.getElementById("addModerator");
-const deleteModerator = document.getElementById("deleteModerator");
 const showTable = document.getElementById("showTable");
 const showUsers = document.getElementById("showUsers");
 const showModerators = document.getElementById("showModerators");
 const query = firebase.database().ref("users");
+const db = firebase.database();
 const all = document.querySelectorAll("body");
 
-firebase.auth().onAuthStateChanged(function(user) {
+firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
-      firebase.database().ref('/users/' + user.uid).once('value').then(function(snapshot) {
+    firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
       var tipKorisnika = snapshot.val().tipKorisnika;
-      if(tipKorisnika == 99){
+      if (tipKorisnika == 99) {
         all.forEach(item => item.style.display = 'block');
-      }else{
+      } else {
         window.location = "./404.html";
       }
-});
-      
+    });
+
   } else {
-      window.location = "./404.html";
+    window.location = "./404.html";
   }
 });
 
-addModerator.addEventListener('click',function(){
+let adminPanelData = () => {
+  query.once('value', (snap) => {
+    let numOfUsers = snap.numChildren();
 
-        let emailInput = document.getElementById("addModeratorEmail").value;
-        var query = firebase.database().ref("users")
-        query.once("value")
-          .then(function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
+    document.getElementById("numOfUsers").innerHTML = numOfUsers;
+  });
 
-              var email = childSnapshot.child("email").val();
-              var userId= childSnapshot.child("uid").val();
-              var username = childSnapshot.child("username").val();
-            
-              if(emailInput){
-                var tipKorisnika = childSnapshot.child("tipKorisnika").val();
-                if(emailInput == email){ 
-              
-                switch(tipKorisnika) {
-                    case 10:
-                        emailInput = null;
-                        document.getElementById("addModeratorEmail").value = null;
-                        alert(email+" je već moderator");
-                        break;
-                    case 99:
-                        emailInput = null;
-                        document.getElementById("addModeratorEmail").value = null;
-                        alert(email+" je admin i ne možete mu mijenjati vrijednosti");
-                        
-                        break;
-                    case 0:
-                        emailInput = null;
-                        document.getElementById("addModeratorEmail").value = null;
-                        firebase.database().ref('users/'+userId).set({
-                            email:email,
-                            tipKorisnika:10,
-                            uid:userId,
-                            username:username  
-                        })
-                  
-                        
-                        alert("Uspješno dodan moderator na "+email);
-                        break;
-                        }   
-                    }
-                }
-              
-          });
-        });
+  db.ref("events").once('value', (snap) => {
+    let numOfEvents = snap.numChildren();
 
+    document.getElementById("numOfEvents").innerHTML = numOfEvents;
+  });
+};
 
-    
+let listUsers = () => {
+  let query = firebase.database().ref("users");
+  let allModerators = [];
+  let allAdmins = [];
+  let allUsers = [];
+  query.on("value", snapshot => {
+    snapshot.forEach((childSnapshot) => {
 
-function errData(err){
-    console.log(err);
-}
+      childSnapshot.child("tipKorisnika").val() === 99 ?
+        allAdmins.push(childSnapshot.val()) && allUsers.push(childSnapshot.val()) && allModerators.push(childSnapshot.val()) :
+        (childSnapshot.child("tipKorisnika").val() === 10 ?
+          allModerators.push(childSnapshot.val()) && allUsers.push(childSnapshot.val()) :
+          allUsers.push(childSnapshot.val()));
+    });
+  });
 
-})
+  new Vue({
+    el: "#listUsers",
+    data: {
+      users: allUsers,
+    }
+  });
 
+  new Vue({
+    el: "#listAdmins",
+    data: {
+      admins: allAdmins,
+    }
+  });
 
-deleteModerator.addEventListener('click',function(){
+  new Vue({
+    el: "#listModerators",
+    data: {
+      moderators: allModerators,
+    }
+  });
+};
 
-    let emailInput = document.getElementById("addModeratorEmail").value;
+let refreshTable = () => {
+  document.getElementById("moderatorTableBody").innerHTML = "";
+  document.getElementById("adminTableBody").innerHTML = "";
+  document.getElementById("userTableBody").innerHTML = "";
+};
+
+let deleteModerator = () => {
+  let emailInput = document.getElementById("editModeratorInput").value;
+  let successAlert = document.getElementById("moderatorSuccessAlert");
+  let errorAlert = document.getElementById("moderatorErrorAlert");
+
+  if (emailInput) {
+    let counter = 0;
+
     var query = firebase.database().ref("users")
     query.once("value")
-      .then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
+      .then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
 
           var email = childSnapshot.child("email").val();
-          var userId= childSnapshot.child("uid").val();
+          var userId = childSnapshot.child("uid").val();
           var username = childSnapshot.child("username").val();
-        
-          if(emailInput){
-            var tipKorisnika = childSnapshot.child("tipKorisnika").val();
-            if(emailInput == email){ 
-          
-            switch(tipKorisnika) {
-                case 0:
-                    emailInput = null;
-                    document.getElementById("addModeratorEmail").value = null;
-                    alert(email+" nije moderator");
-                    break;
-                case 99:
-                    emailInput = null;
-                    document.getElementById("addModeratorEmail").value = null;
-                    alert(email+" je admin i ne možete mu mijenjati vrijednosti");
-                    
-                    break;
-                case 10:
-                    emailInput = null;
-                    document.getElementById("addModeratorEmail").value = null;
-                    firebase.database().ref('users/'+userId).set({
-                        email:email,
-                        tipKorisnika:0,
-                        uid:userId,
-                        username:username  
-                    })
-              
-                    
-                    alert("Uspješno izbrisan moderator na "+email);
-                    break;
-                    }   
-                }
-            }
-          
+
+          var tipKorisnika = childSnapshot.child("tipKorisnika").val();
+          if (emailInput == email) {
+            counter++;
+
+            switch (tipKorisnika) {
+              case 0:
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+
+                successAlert.style.display = "none"
+                errorAlert.innerHTML = `Korisnik <b>${email}</b> nije moderator!`;
+                errorAlert.style.display = "block";
+                break;
+              case 99:
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+
+                successAlert.style.display = "none"
+                errorAlert.innerHTML = `Korisnik <b>${email}</b> je administrator i ne možete mu mijenjati vrijednosti!`;
+                errorAlert.style.display = "block";
+                break;
+              case 10:
+                refreshTable();
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+                firebase.database().ref('users/' + userId).set({
+                  email: email,
+                  tipKorisnika: 0,
+                  uid: userId,
+                  username: username
+                });
+
+                errorAlert.style.display = "none"
+                successAlert.innerHTML = `Korisniku <b>${email}</b> je uspješno uklonjena uloga moderatora!`;
+                successAlert.style.display = "block";
+                break;
+            };
+          };
+        });
+
+        if (!counter) {
+          successAlert.style.display = "none"
+          errorAlert.innerHTML = `Nije pronađen ovaj korisnik. Provjerite unos!`;
+          errorAlert.style.display = "block";
+        };
+
       });
-    });
+  } else {
+    successAlert.style.display = "none"
+    errorAlert.innerHTML = `Unesite email!`;
+    errorAlert.style.display = "block";
+  };
+};
+
+let addModerator = () => {
 
 
+  let emailInput = document.getElementById("editModeratorInput").value;
+  let successAlert = document.getElementById("moderatorSuccessAlert");
+  let errorAlert = document.getElementById("moderatorErrorAlert");
+
+  if (emailInput) {
+
+    let counter = 0;
+
+    var query = firebase.database().ref("users");
+    query.once("value")
+      .then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+
+          let email = childSnapshot.child("email").val();
+          let userId = childSnapshot.child("uid").val();
+          let username = childSnapshot.child("username").val();
 
 
-function errData(err){
-console.log(err);
-}
+          let tipKorisnika = childSnapshot.child("tipKorisnika").val();
+          if (emailInput == email) {
+            counter++;
 
-})
+            switch (tipKorisnika) {
+              case 10:
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+
+                successAlert.style.display = "none"
+                errorAlert.innerHTML = `Korisnik <b>${email}</b> je već moderator!`;
+                errorAlert.style.display = "block";
+                break;
+              case 99:
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+
+                successAlert.style.display = "none"
+                errorAlert.innerHTML = `Korisnik <b>${email}</b> je administrator i ne možete mu mijenjati vrijednosti!`;
+                errorAlert.style.display = "block";
+                break;
+              case 0:
+                refreshTable();
+                emailInput = null;
+                document.getElementById("editModeratorInput").value = null;
+                firebase.database().ref('users/' + userId).set({
+                  email: email,
+                  tipKorisnika: 10,
+                  uid: userId,
+                  username: username
+                });
 
 
+                errorAlert.style.display = "none"
+                successAlert.innerHTML = `Korisnik <b>${email}</b> je uspješno promoviran u moderatora!`;
+                successAlert.style.display = "block";
+                break;
+            };
+          };
 
 
-query.on("value",snapshot => {
-    var myTable = "<table border='1' width='100%'><tr>";
-    myTable += "<th align='center'>" + "Ime"+ "</th>";
-    myTable += "<th align='center'>" + "Email"+ "</th>";
-    myTable += "<th align='center'>" + "Tip korisnika"+ "</th>";
-    myTable += "</tr>";
+        });
 
-    var myTable1 = "<table border='1' width='100%'><tr>";
-    myTable1 += "<th align='center'>" + "Ime"+ "</th>";
-    myTable1 += "<th align='center'>" + "Email"+ "</th>";
-    myTable1 += "</tr>";
+        if (!counter) {
+          successAlert.style.display = "none"
+          errorAlert.innerHTML = `Nije pronađen ovaj korisnik. Provjerite unos!`;
+          errorAlert.style.display = "block";
+        };
 
-    var myTable2 = "<table border='1' width='100%'><tr>";
-    myTable2 += "<th align='center'>" + "Ime"+ "</th>";
-    myTable2 += "<th align='center'>" + "Email"+ "</th>";
-    myTable2 += "</tr>";
-
-    snapshot.forEach(function(childSnapshot) {
-
-      var email = childSnapshot.child("email").val();
-      var username = childSnapshot.child("username").val();
-      var tipKorisnika = childSnapshot.child("tipKorisnika").val();
-      var userId = childSnapshot.child("uid").val();
-      myTable += "<td align='center'>" + username +"</td>";
-      myTable += "<td align='center'>" + email +"</td>";
-      if(tipKorisnika == 0){
-        myTable += `<td align='center'>` + "Korisnik" +"</td>";
-      }
-      if(tipKorisnika == 10){
-        myTable += `<td align='center'>` + "Moderator" +"</td>";
-      }
-      if(tipKorisnika == 99){
-        myTable += `<td align='center'>` + "Admin" +"</td>";
-      }
-
-      myTable += "</tr>";
-      
-      if(tipKorisnika == 0){
-        myTable1 += "<td align='center'>" + username +"</td>";
-        myTable1 += "<td align='center'>" + email +"</td>";
-        myTable1 += "</tr>";
-      }
-
-      if(tipKorisnika == 10){
-        myTable2 += "<td align='center'>" + username +"</td>";
-        myTable2 += "<td align='center'>" + email +"</td>";
-        myTable2 += "</tr>";
-      }
       });
-      myTable += "</tr></table>";
-      myTable1 += "</tr></table>";
-      myTable2 += "</tr></table>";
 
-      document.getElementById("outputDiv").innerHTML = myTable;
-      document.getElementById("outputDivUser").innerHTML = myTable1;
-      document.getElementById("outputDivModerator").innerHTML = myTable2;
+  } else {
+    successAlert.style.display = "none"
+    errorAlert.innerHTML = `Unesite email!`;
+    errorAlert.style.display = "block";
+  };
 
-      showTable.addEventListener('click',function(){
-        document.getElementById("outputDiv").style.display = "block";
-        document.getElementById("outputDivUser").style.display = "none";
-        document.getElementById("outputDivModerator").style.display = "none";
-    });
+};
 
-    showUsers.addEventListener('click',function(){
-        document.getElementById("outputDiv").style.display = "none";
-        document.getElementById("outputDivUser").style.display = "block";
-        document.getElementById("outputDivModerator").style.display = "none";
-    });
-
-    showModerators.addEventListener('click',function(){
-        document.getElementById("outputDiv").style.display = "none";
-        document.getElementById("outputDivUser").style.display = "none";
-        document.getElementById("outputDivModerator").style.display = "block";
-    });
-})
-
+listUsers();
+adminPanelData();
--- public/index.js
+++ public/index.js
@@ -1,138 +1,143 @@
 firebase.auth().onAuthStateChanged(function (user) {
-   
-    if (user) {
-        setupUI(user);
-    }else {
-        setupUI();
-    }
+
+  if (user) {
+    setupUI(user);
+  } else {
+    setupUI();
+  }
 });
 
 
 function uploadImage() {
-    // File or Blob named mountains.jpg
-    const file = document.querySelector("#photo").files[0];
-    var user = firebase.auth().currentUser;
-    const name = user.uid;
-    var uploader = document.getElementById("uploader");
+  // File or Blob named mountains.jpg
+  const file = document.querySelector("#userProfileImageInput").files[0];
+  let user = firebase.auth().currentUser;
+  const name = user.uid;
+  let errorAlert = document.getElementById("userProfileImageUploadError");
+  let successAlert = document.getElementById("userProfileImageUploadSuccess");
+  let uploadSpinner = document.getElementById("userProfilePhotoSpinner");
+  let uploadProgress = document.getElementById("userProfilePhotoProgress");
+  uploadSpinner.style.display = "block";
 
-// Create the file metadata
-var metadata = {
-  contentType: file.type
-};
+  errorAlert.style.display = "none";
+  successAlert.style.display = "none";
 
-// Upload file and metadata to the object 'images/mountains.jpg'
-var uploadTask = storageRef.child('userImages/' + name).put(file, metadata);
+  if (!file) {
+    errorAlert.innerHTML = "Dogodila se greška: Nije odabrana datoteka!";
+    errorAlert.style.display = "block";
+  } else {
+    errorAlert.style.display = "none";
 
-// Listen for state changes, errors, and completion of the upload.
-uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-  function(snapshot) {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    
-    uploader.value  = progress;
-    switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-        break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-        break;
-    }
-  }, function(error) {
+    // Create the file metadata
+    var metadata = {
+      contentType: file.type
+    };
 
-  // A full list of error codes is available at
-  // https://firebase.google.com/docs/storage/web/handle-errors
-  switch (error.code) {
-    case 'storage/unauthorized':
-      // User doesn't have permission to access the object
-      break;
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    var uploadTask = storageRef.child('userImages/' + name).put(file, metadata);
 
-    case 'storage/canceled':
-      // User canceled the upload
-      break;
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+      function (snapshot) {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        let uploadedPercent = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
 
-    case 'storage/unknown':
-      // Unknown error occurred, inspect error.serverResponse
-      break;
-  }
-}, function() {
-  // Upload completed successfully, now we can get the download URL
-  uploadTask.snapshot.ref.getDownloadURL().then(function(url) {
-     document.querySelector("#profilePhoto").src = url;
-     uploader.value  = 0;
-     var imgUrl = url;
-     user.updateProfile({
-        photoURL:imgUrl
+        uploadProgress.innerHTML = `${uploadedPercent}% učitano`;
+
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      }, function (error) {
+
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            // User doesn't have permission to access the object
+            errorAlert.innerHTML = "Korisniku nije dopušten pristup objektu!";
+            errorAlert.style.display = "block";
+            break;
+
+          case 'storage/canceled':
+            // User canceled the upload
+            errorAlert.innerHTML = "Korisnik je prekinio prijenos datoteka!";
+            errorAlert.style.display = "block";
+            break;
+
+          case 'storage/unknown':
+            // Unknown error occurred, inspect error.serverResponse
+            errorAlert.innerHTML = "Nepoznata greška. Pokušajte ponovno!";
+            errorAlert.style.display = "block";
+            break;
+          default:
+            console.log("Error: " + error.code);
+            break;
+        }
+      }, function () {
+        // Upload completed successfully, now we can get the download URL
+        uploadTask.snapshot.ref.getDownloadURL().then(function (url) {
+          document.querySelector("#profilePhoto").src = url;
+
+          errorAlert.style.display = "none";
+          uploadSpinner.style.display = "none";
+          uploadProgress.style.display = "none";
+          document.getElementById("addUserProfileImageButton").style.display = "none";
+
+
+          let imgUrl = url;
+          user.updateProfile({
+            photoURL: imgUrl
+          });
+        }).then(() => {
+          // window.alert("Uspješan upload!")
+          successAlert.style.display = "block";
+          successAlert.innerHTML = "Uspješno promijenjena slika!";
+          document.getElementById("uploadImageButton").style.display = "none";
         });
-  }).then(function() {
-    window.alert("Uspješan upload!")
-}).then(function(){
-    window.location = './index.html'; //After successful login, user will be redirected to index.html
-})
-    
-});
-        
-    }
 
-function showPreview(event){
-    if(event.target.files.length > 0){
-        var src =URL.createObjectURL(event.target.files[0]);
-        var preview = document.getElementById("profilePhoto"); //dodati id gdje ce biti preview slike
-        preview.src = src;
-    }
-}
-
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+      });
   }
+
+
+
 }
-function postovi(){ // samo logovi od eventova za postove
+
+function postovi() { // samo logovi od eventova za postove
   var query = firebase.database().ref("events");
-  query.on("value",(data)=>{
-    var posts = data.val();
-    
-   
-    document.getElementById("allEvents").innerHTML = ''
-    for(const post in posts){
-         if(posts[post].objectName == undefined){
-      document.getElementById("allEvents").innerHTML +=
-      `<tr>
-      <td>${posts[post].eventName}</td>
-      <td>${posts[post].description}</td>
-      <td>${posts[post].eventDate}</td>
-      <td>${posts[post].eventTime}</td>
-      <td>${posts[post].eventImgUrl} </td>
-      <tr>`
-    }else{
-      document.getElementById("allEvents").innerHTML +=
-      `<tr>
-      <td>${posts[post].eventName} </td>
-      <td>${posts[post].description}</td>
-      <td>${posts[post].eventDate} </td>
-      <td>${posts[post].eventTime}</td>
-      <td>${posts[post].objectName}</td>
-      <td><img src="${posts[post].eventImgUrl}"></img></td>
-      <tr>`
+  query.on("value", (data) => {
+    let posts = data.val();
+
+    if (!posts) {
+      document.getElementById("eventsError").style.display = "block";
+      document.getElementById("eventList").style.display = "none";
+      document.getElementById("eventsError").innerHTML = "Nažalost, nema eventova koje vam možemo prikazati. Pokušajte ponovno kasnije!";
     }
-    }
+
+    new Vue({
+      el: "#eventList",
+      data: {
+        eventsData: posts,
+      },
+    });
 
   })
 }
 postovi();
 
+let editUserImage = () => {
+
+  let addImageButton = document.getElementById("addUserProfileImageButton");
+  let imagePreview = document.getElementById('userProfileImageInputPreview');
+  let fileInput = document.getElementById("userProfileImageInput");
+
+  imagePreview.style.height = imagePreview.style.width.value;
+  imagePreview.style.display = "none";
+  addImageButton.style.display = "none";
+  fileInput.value = null;
+  fileInput.style.display = "block";
+};
